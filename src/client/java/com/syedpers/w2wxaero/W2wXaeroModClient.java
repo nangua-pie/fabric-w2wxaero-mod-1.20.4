34c30e68f82b868ab71bd76de10b3bfff5d8e5f8
Syed <pxqm@qq.com>
2024-03-07 14:16:24 +0800

    add update and remove handle

--- src/client/java/com/syedpers/w2wxaero/W2wXaeroModClient.java
+++ src/client/java/com/syedpers/w2wxaero/W2wXaeroModClient.java
@@ -12,6 +12,9 @@ import net.blay09.mods.waystones.api.Waystone;
 import net.blay09.mods.waystones.api.event.WaystoneActivatedEvent;
 import net.blay09.mods.waystones.api.event.WaystoneInitializedEvent;
 import net.blay09.mods.waystones.api.event.WaystoneRemovedEvent;
+import net.blay09.mods.waystones.api.event.WaystoneUpdateReceivedEvent;
+import net.blay09.mods.waystones.api.event.WaystoneUpdatedEvent;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,17 +23,11 @@ public class W2wXaeroModClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		Balm.getEvents().onEvent(WaystoneActivatedEvent.class, this::onWaystoneActivated);
-		Balm.getEvents().onEvent(WaystoneInitializedEvent.class, null);
+		Balm.getEvents().onEvent(WaystoneUpdatedEvent.class, this::onWaystoneUpdated);
+		Balm.getEvents().onEvent(WaystoneRemovedEvent.class, this::onWaystoneRemoved);
 		//WaystonesListReceivedEvent
 		//WaystonesRemoveReceivedEvent
 		//WaystonesUpdateReceivedEvent
-		Balm.getEvents().onEvent(WaystoneRemovedEvent.class, this::onWaystoneRemoved);
-	}
-	
-	public List<Waystone> currentWaystones = new ArrayList<>();
-
-	public void subscribeWaystonesEvent(WaystoneActivatedEvent waystoneActivatedEvent) {
-		
 	}
 
 	public void onWaystoneActivated(WaystoneActivatedEvent waystoneActivatedEvent) {
@@ -40,7 +37,7 @@ public class W2wXaeroModClient implements ClientModInitializer {
 			WaypointsManager waypointsManager = minimapSession.getWaypointsManager();
 			BlockPos pos=waystoneActivatedEvent.getWaystone().getPos();
 			String name = waystoneActivatedEvent.getWaystone().getName().getString();
-			if (name == null || name == "") name = "Untitled Waystone";
+			if (name == null || name.isEmpty()) name = "Untitled Waystone";
 			Waypoint waypoint = new Waypoint(pos.getX(), pos.getY()+2, pos.getZ(), name, name.substring(0,1),(int)(Math.random()*ModSettings.ENCHANT_COLORS.length));
 			waypointsManager.getWaypoints().getList().add(waypoint);
 			XaeroMinimap.instance.getSettings().saveWaypoints(waypointsManager.getCurrentWorld());
@@ -48,5 +45,50 @@ public class W2wXaeroModClient implements ClientModInitializer {
 		}
 	}
 
-	public void onWaystoneRemoved(WaystoneRemovedEvent waystoneRemovedEvent) {}
+	public void onWaystoneRemoved(WaystoneRemovedEvent waystoneRemovedEvent) {
+		try {
+			XaeroMinimapSession minimapSession = XaeroMinimapSession.getCurrentSession();
+			if (minimapSession == null) return;
+			WaypointsManager waypointsManager = minimapSession.getWaypointsManager();
+			Waypoint waypoint = getWaypoint(waypointsManager.getWaypoints().getList(), waystoneRemovedEvent.getWaystone());
+			waypointsManager.getWaypoints().getList().remove(waypoint);
+			XaeroMinimap.instance.getSettings().saveWaypoints(waypointsManager.getCurrentWorld());
+		} catch(final Exception e) {
+		}
+	}
+
+	public void onWaystoneUpdated(WaystoneUpdatedEvent waystoneUpdatedEvent) {
+		try {
+			XaeroMinimapSession minimapSession = XaeroMinimapSession.getCurrentSession();
+			if (minimapSession == null) return;
+			WaypointsManager waypointsManager = minimapSession.getWaypointsManager();
+			Waystone waystone = waystoneUpdatedEvent.getWaystone();
+			BlockPos pos=waystone.getPos();
+			String name = waystone.getName().getString();
+			if (name == null || name.isEmpty()) name = "Untitled Waystone";
+			Waypoint waypoint = getWaypoint(waypointsManager.getWaypoints().getList(), waystone);
+			if (waypoint == null) {
+				waypoint = new Waypoint(pos.getX(), pos.getY()+2, pos.getZ(), name, name.substring(0,1),(int)(Math.random()*ModSettings.ENCHANT_COLORS.length));
+				waypointsManager.getWaypoints().getList().add(waypoint);
+			} else {
+				waypoint.setName(name);
+				waypoint.setSymbol(name.substring(0, 1));
+				waypoint.setX(pos.getX());
+				waypoint.setY(pos.getY() + 2);
+				waypoint.setZ(pos.getZ());
+			}
+			XaeroMinimap.instance.getSettings().saveWaypoints(waypointsManager.getCurrentWorld());
+		} catch(final Exception e) {
+		}
+	}
+
+	private Waypoint getWaypoint(ArrayList<Waypoint> waypoints, Waystone waystone){
+        BlockPos waystonePos = waystone.getPos();
+        for(Waypoint waypoint : waypoints){
+            if(waypoint.getX() == waystonePos.getX() && waypoint.getY() == waystonePos.getY() + 2 && waypoint.getZ() == waystonePos.getZ()){
+                return waypoint;
+            }
+        }
+        return null;
+    }
  }
